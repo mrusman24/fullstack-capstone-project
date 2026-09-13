8e483ac6a7182dc63648e036e10ca8dae1711b1a
mrusman24 <mrusman24@github.com>
2026-09-13 23:47:38 +0500

    fix: remove unused imports (ESLint warnings in App.js and AuthContext.js)

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import SearchPage from './components/SearchPage/SearchPage';
